f8857230b88234db525ba8572567b95ed045a235
Anton15K <anton.kogun@icloud.com>
2026-01-13 10:45:51 +0200

    created publisher

--- 2/src/example_package/example_package/example_node.py
+++ 2/src/example_package/example_package/example_node.py
@@ -2,6 +2,7 @@
 import os
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 # Fill in something for msg type imports
 # from duckietown_msgs.msg import SOMETHING
@@ -10,10 +11,15 @@ from rclpy.node import Node
 class SkeletonNode(Node):
     def __init__(self):
         super().__init__('example_node')
-        #Create publishers and subscribers in init, use callback
-        pass
-    
-    #Define callback functions here
+        self.pub = self.create_publisher(String, 'chatter', 10)  # create publisher
+        self.i = 0
+        self.timer = self.create_timer(1.0, self.publish_msgs)
+
+    def publish_msgs(self):
+        msg = String()
+        msg.data = 'Hello World' + f"{self.i}"
+        self.pub.publish(msg)
+        self.i += 1
 
 
 def main():
